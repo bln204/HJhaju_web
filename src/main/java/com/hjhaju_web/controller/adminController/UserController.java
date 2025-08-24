31a7a6027551869177bc460bc0560ea5b860d409
thangnguyen <thangnbnt2004@gmail.com>
2025-08-25 00:54:19 +0700

    third commit

---
+++ src/main/java/com/hjhaju_web/controller/adminController/UserController.java
@@ -0,0 +1,15 @@
+package com.hjhaju_web.controller.adminController;
+
+import com.hjhaju_web.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin/dashboard/show";
+    }
+}
